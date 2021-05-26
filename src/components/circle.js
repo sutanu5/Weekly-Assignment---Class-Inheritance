5889a3cc5684c989822dfa67d2d8fb86d560abea
Sutanu Sain <sutanu.sain@gmail.com>
2021-05-26 19:46:40 +0530

    Update circle.js

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,27 @@
-// import using require
-
-// declare class
-
-
 // export class using module.exports
+
+import React, { Component } from 'react';
+
+
+ 
+
+import shape from './shape'
+
+class circle extends shape {
+
+    constructor(props) {
+
+      super(props);
+
+  }
+
+    render() {
+
+      return <div>{this.calculateArea()}</div>;
+
+    }
+
+  }
+
+  export default circle;
+
