34ec298433013173715c25898f20017173074142
Sutanu Sain <sutanu.sain@gmail.com>
2021-05-26 19:48:06 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,50 @@
 // declare class
 
 // export class using module.exports
+import React, { Component } from "react";
+
+
+ 
+
+class shape extends React.Component {
+
+  constructor(props) {
+
+    super(props);
+
+    this.state = { color: "red" };
+
+  }
+
+  drawShape() {
+
+    return "shape";
+
+  }
+
+  calculateArea() {
+
+    return "Area";
+
+  }
+
+  render() {
+
+    return (
+
+      <div>
+
+        Area:{this.calculateArea()} shape:{this.drawShape()}
+
+      </div>
+
+    );
+
+  }
+
+}
+
+
+ 
+
+export default shape
