f661f0f008ea210697ac6c5f1f3f1d034a48e76e
Sutanu Sain <sutanu.sain@gmail.com>
2021-05-27 17:38:53 +0530

    Create App.js

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+
+  
+import React from "react";
+
+var Circle = require('./Circle');
+
+class App extends React{
+
+   constructor(side){
+     super();
+     this.side = side;
+   }
+
+}
+
+export default App;
